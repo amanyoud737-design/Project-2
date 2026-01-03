942e4f30ef3a94b37cf29a32c680b365be5db172
amanyoud737-design <amanyoud737@gmail.com>
2026-01-03 16:19:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const CURRENCY = process.env.CURRENCY || "USD";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const frontendDir = __dirname;
+const frontendDir = process.cwd();
+
 
 
 // Basic middleware
